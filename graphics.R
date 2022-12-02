8833c7e598c62d3e8a47edff971fe3bf1575569b
lbenz730 <lukesbenz@gmail.com>
2022-12-02 16:26:24 -0500

    Tournament Update 2022-12-02

--- graphics.R
+++ graphics.R
@@ -14,6 +14,20 @@ df_stats <-
   read_csv('predictions/sim_results.csv') %>% 
   mutate('logo' = paste0('flags/', team, '.png')) 
 
+schedule <- 
+  read_csv('data/schedule.csv')
+
+df_r16 <- 
+  schedule %>% 
+  filter(grepl('R16', ko_round))
+bracket1 <- c(df_r16$team1[1:4], df_r16$team2[1:4])
+bracket2 <- c(df_r16$team1[1:4], df_r16$team2[1:4])
+eliminated_teams <- history$team[history$date == max(history$date) & history$r16 == 0]
+
+history_ko <- 
+  history %>% 
+  filter(!team %in% eliminated_teams)
+
 
 theme_set(theme_bw() + 
             theme(plot.title = element_text(size = 24, hjust = 0.5),
@@ -44,7 +58,6 @@ ggplot(history, aes(x = date, y = r16)) +
        y = 'Chances of Reaching Knockout Round',
        title = 'World Cup 2022',
        subtitle = 'Knockout Round Chances Over Time')
-
 ggsave('figures/r16.png', height = 12/1.2, width = 16/1.2)
 
 ggplot(history, aes(x = date, y = qf)) +
@@ -66,6 +79,25 @@ ggplot(history, aes(x = date, y = qf)) +
 
 ggsave('figures/qf.png', height = 12/1.2, width = 16/1.2)
 
+ggplot(history_ko, aes(x = date, y = qf)) +
+  facet_wrap(~ifelse(team %in% bracket1, 'Bracket 1', 'Bracket 2'), ncol = 2) +
+  geom_line(aes(group = team), col = 'black', alpha = 0.4) +
+  geom_image(data = filter(history_ko, !eliminated), 
+             aes(image = logo),
+             size = 0.085) + 
+  geom_image(data = filter(history_ko, eliminated), 
+             aes(image = logo),
+             image_fun = transparent, 
+             size = 0.085) + 
+  scale_y_continuous(limits = c(0,1), labels = scales::percent) + 
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = 'Date',
+       y = 'Chances of Reaching Quarterfinals',
+       title = 'World Cup 2022',
+       subtitle = 'QF Chances Over Time')
+
+ggsave('figures/qf_by_bracket.png', height = 12/1.2, width = 16/1.2)
+
 ggplot(history, aes(x = date, y = sf)) +
   facet_wrap(~paste('Group', group), ncol = 4) +
   geom_line(aes(group = team), col = 'black', alpha = 0.4) +
@@ -84,6 +116,25 @@ ggplot(history, aes(x = date, y = sf)) +
        subtitle = 'SF Chances Over Time')
 
 ggsave('figures/sf.png', height = 12/1.2, width = 16/1.2)
+
+ggplot(history_ko, aes(x = date, y = sf)) +
+  facet_wrap(~ifelse(team %in% bracket1, 'Bracket 1', 'Bracket 2'), ncol = 2) +
+  geom_line(aes(group = team), col = 'black', alpha = 0.4) +
+  geom_image(data = filter(history_ko, !eliminated), 
+             aes(image = logo),
+             size = 0.085) + 
+  geom_image(data = filter(history_ko, eliminated), 
+             aes(image = logo),
+             image_fun = transparent, 
+             size = 0.085) + 
+  scale_y_continuous(limits = c(0,1), labels = scales::percent) + 
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = 'Date',
+       y = 'Chances of Reaching Semifinals',
+       title = 'World Cup 2022',
+       subtitle = 'SF Chances Over Time')
+
+ggsave('figures/sf_by_bracket.png', height = 12/1.2, width = 16/1.2)
 
 ggplot(history, aes(x = date, y = finals)) +
   facet_wrap(~paste('Group', group), ncol = 4) +
@@ -104,6 +155,25 @@ ggplot(history, aes(x = date, y = finals)) +
 
 ggsave('figures/finals.png', height = 12/1.2, width = 16/1.2)
 
+ggplot(history_ko, aes(x = date, y = finals)) +
+  facet_wrap(~ifelse(team %in% bracket1, 'Bracket 1', 'Bracket 2'), ncol = 2) +
+  geom_line(aes(group = team), col = 'black', alpha = 0.4) +
+  geom_image(data = filter(history_ko, !eliminated), 
+             aes(image = logo),
+             size = 0.085) + 
+  geom_image(data = filter(history_ko, eliminated), 
+             aes(image = logo),
+             image_fun = transparent, 
+             size = 0.085) + 
+  scale_y_continuous(limits = c(0,1), labels = scales::percent) + 
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = 'Date',
+       y = 'Chances of Reaching Finals',
+       title = 'World Cup 2022',
+       subtitle = 'Finals Chances Over Time')
+
+ggsave('figures/finals_by_bracket.png', height = 12/1.2, width = 16/1.2)
+
 
 ggplot(history, aes(x = date, y = champ)) +
   facet_wrap(~paste('Group', group), ncol = 4) +
@@ -123,6 +193,25 @@ ggplot(history, aes(x = date, y = champ)) +
 
 ggsave('figures/champ.png', height = 12/1.2, width = 16/1.2)
 
+ggplot(history_ko, aes(x = date, y = champ)) +
+  facet_wrap(~ifelse(team %in% bracket1, 'Bracket 1', 'Bracket 2'), ncol = 2) +
+  geom_line(aes(group = team), col = 'black', alpha = 0.4) +
+  geom_image(data = filter(history_ko, !eliminated), 
+             aes(image = logo),
+             size = 0.085) + 
+  geom_image(data = filter(history_ko, eliminated), 
+             aes(image = logo),
+             image_fun = transparent, 
+             size = 0.085) + 
+  scale_y_continuous(limits = c(0,1), labels = scales::percent) + 
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(x = 'Date',
+       y = 'Chances of Winning Tournament',
+       title = 'World Cup 2022',
+       subtitle = 'Title Chances Over Time')
+
+ggsave('figures/champ_by_bracket.png', height = 12/1.2, width = 16/1.2)
+
 
 df_elim <- 
   df_stats %>% 
@@ -140,7 +229,7 @@ df_elim <-
                values_to = 'elim_prob') %>% 
   mutate('elim_round' = factor(elim_round, levels = c('Group', 'R16', 'QF', 'SF', 'Final', 'Champ'))) %>% 
   mutate('team' = fct_reorder(team, desc(expected_round)))
-  
+
 
 labels <- paste0("<img src ='", unique(df_elim %>% arrange(-expected_round) %>% pull(logo)), "', width = '20'/>")
 
@@ -235,8 +324,8 @@ match3_plot <- function(group_, score_max, colors) {
 # match3_plot('B', score_max = 2, c('#2B57AC', '#239F40', '#0A3161', '#C8102E'))
 # match3_plot('C', score_max = 2, c('#6CACE4', '#006341', '#C8102E', '#165d31'))
 # match3_plot('D', score_max = 2, c('#FFF200', '#C60C30', '#002654', '#C8102E'))
-match3_plot('E', score_max = 2, c('#002654', '#000000', '#BC002D', '#F1BF00'))
-match3_plot('F', score_max = 2, c('#FFCD00', '#D80621', '#012169', '#C1272D'))
-# match3_plot('G', score_max = 2, c('#2B57AC', '#239F40', '#0A3161', '#C8102E'))
-# match3_plot('H', score_max = 2, c('#FFD100', '#FFA500', '#8A1538', '#00853F'))
+# match3_plot('E', score_max = 2, c('#002654', '#000000', '#BC002D', '#F1BF00'))
+# match3_plot('F', score_max = 2, c('#FFCD00', '#D80621', '#012169', '#C1272D'))
+# match3_plot('G', score_max = 2, c('#FEDD00', '#007A5E', '#0000FF', '#DA291C'))
+# match3_plot('H', score_max = 2, c('#FFD100', '#046A38', '#CD2E3A', '#001489'))
 
